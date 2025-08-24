bb88e0a4bf2b2bdfb6c1b51fd990d0b89fa13e42
Narayandas Kachhi <126740162+narayan24x7@users.noreply.github.com>
2025-08-24 04:16:30 +0000

    Initial commit

--- utils/planData.jsx
+++ utils/planData.jsx
@@ -1,47 +1,27 @@
-export default[
-    {
-        id:1,
-        name:'Free',
-        cost:0,
-        offering:[
-            {
-                value:'✔️ Create 3 Free Mock Interview'
-            },
-            {
-                value:'✔️ Unlimited Retake Interview'
-            },
-            {
-                value:'❌ Practice Question'
-            },
-            {
-                value:'❌ NkTechInfo.Com Exlcusive App Access'
-            },
-            {
-                value:'❌ Email Support'
-            },
-        ]
-    },
-    {
-        id:1,
-        name:'Monthly',
-        cost:7.99,
-        paymentLink:'https://buy.stripe.com/test_28o9EjbkvgfQ8VOeUV',
-        offering:[
-            {
-                value:'✔️ Create 3 Free Mock Interview'
-            },
-            {
-                value:'✔️ Unlimited Retake Interview'
-            },
-            {
-                value:'✔️ Practice Question'
-            },
-            {
-                value:'✔️ NkTechInfo.Com Exlcusive App Access'
-            },
-            {
-                value:'✔️ Email Support'
-            }
-        ]
-    }
-]
+export default [
+  {
+    id: 1,
+    name: "Free",
+    cost: 0,
+    offering: [
+      { value: "✔️ Create 5 Free Mock Interviews" },
+      { value: "✔️ Unlimited Retake Interviews" },
+      { value: "❌ Practice Questions" },
+      { value: "❌ NkTechInfo.com Exclusive App Access" },
+      { value: "❌ Email Support" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Monthly",
+    cost: 7.99,
+    paymentLink: "https://buy.stripe.com/test_28o9EjbkvgfQ8VOeUV",
+    offering: [
+      { value: "✔️ Create Unlimited Mock Interviews" },
+      { value: "✔️ Unlimited Retake Interviews" },
+      { value: "✔️ Practice Questions" },
+      { value: "✔️ NkTechInfo.com Exclusive App Access" },
+      { value: "✔️ Email Support" },
+    ],
+  },
+];
